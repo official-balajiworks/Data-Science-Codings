eca8d94f207ceefe3cb1563759190560b67fad55
official-balajiworks <official.balajiworks@gmail.com>
2025-11-09 01:05:20 +0530

    Delete one-hot encoding using pandas.py

--- one-hot encoding using pandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Sample data
-df = pd.DataFrame({'Color': ['Red', 'Blue', 'Green', 'Red']})
-
-# Dummy Variable Encoding
-encoded = pd.get_dummies(df, columns=['Color'], drop_first=True)
-print(encoded)
